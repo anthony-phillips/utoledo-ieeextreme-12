042ea57804043463b34d7c796586cc4a712c819b
anthony-phillips <aphillips196@gmail.com>
2018-10-19 22:27:52 -0400

    added find_node

--- org_chart/org.py
+++ org_chart/org.py
@@ -11,21 +11,31 @@ class node:
         self.all_val = all_val
 
 
-tree = node()
+def query_1(node):
+    pass
 
+def query_2(node):
+    pass
 
-def query(node, query):
-    return None
-
-
-def add_node(new_node, curr_node):
-    if curr_node.name == parent:
-        curr_node.children.add(new_node)
-        return True
+def find_node(curr_node, name):
+    if curr_node.name == name:
+        return curr_node 
 
     for child in curr_node.children:
-        if add_node(new_node, child):
-            return True
+        found_node = find_node(child, name)
+        if found_node:
+            return found_node
+
+    return None 
+    
+
+def add_node(tree, new_node):
+    parent = find_node(tree, new_node.name) 
+
+    if parent:
+        new_node.parent = parent
+        parent.children.add(new_node)
+        return True
 
     return False
 
@@ -33,15 +43,23 @@ def add_node(new_node, curr_node):
 def main():
     n_q = list(input().split())
 
-    for _ in range(n_q[0]):
+    tree = input().split()
+    tree = node(tree[0], tree[1], tree[2])
+
+    for _ in range(1, n_q[0]):
         node_info = input().split()
+        new_node = node(node_info[0], node_info[1], node_info[2])
+        add_node(new_node, tree)
 
     for _ in range(n_q[1]):
-        query_val = input().split()
-
-    new_node.parent = curr_node
-    add_node(new_node, tree)
-
+        query = input().split()
+        query_node = find_node(tree, query[0])
+        query_value = ()
+        if query[1] == 1:
+            query_value = query_1(query_node) 
+        else:
+            query_2(query_node)
+        print(query_value) 
 
 if __name__ == 'main':
     main()
